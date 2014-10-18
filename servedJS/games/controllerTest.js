c3f1e7b06bfa8b5d7ca1c639c95e411b162d3ed2
Thilly <Nicholas.evans1@gmail.com>
2014-10-18 04:54:07 -0400

    finished first pass of basic controllers

--- servedJS/games/controllerTest.js
+++ servedJS/games/controllerTest.js
@@ -5,7 +5,8 @@ window.currentGame = {};
 (function(debug){
 	
 	var controller = {};
-	var canvas;
+	var controllers = ['dPad', 'oneKey' ,'twoKey', 'fourKey' , 'touch',	'stick'];
+	var canvas, controllerIndex = 0;
 	
 	this.init = function(){
 	
@@ -13,29 +14,81 @@ window.currentGame = {};
 		canvas = document.getElementById('gameCanvas');
 		
 		var left = {
-			type: 'dPad',
+			type: controllers[controllerIndex],
 			args: {}
 		};
 		
 		var right = {
-			type: 'dPad',
+			type: 'test',
 			args: {}
 		};
 		
 		controller.create(left, right);
 		
-		canvas.addEventListener('up', function(){
-			console.log('up');
+		//dPad
+		canvas.addEventListener('dPad', function(event){
+			generalHandle('dPad: ' + event.detail.dPad);
 		});
-		canvas.addEventListener('down', function(){
-			console.log('down');
+		
+		//one, two, and fourbutton controllers
+		canvas.addEventListener('button', function(event){
+			generalHandle('button: ' + event.detail.button);
+		});	
+
+		//touchpad
+		canvas.addEventListener('poke', function(){
+			generalHandle('poke');
 		});
-		canvas.addEventListener('left', function(){
-			console.log('left');
+		canvas.addEventListener('touchStart', function(){
+			generalHandle('touchStart');
 		});
-		canvas.addEventListener('right', function(){
-			console.log('right');
-		});		
+		canvas.addEventListener('touchEnd', function(){
+			generalHandle('touchEnd');
+		});
+		canvas.addEventListener('touchMove', function(){
+			generalHandle('touchMove');
+		});	
+		canvas.addEventListener('swipe', function(){
+			generalHandle('swipe');
+		});
+		
+		//stick
+		canvas.addEventListener('angle', function(event){
+			generalHandle('angle: ' + event.detail.angle);
+		});	
+		
+		//testController
+		canvas.addEventListener('next', function(){
+			generalHandle('next');
+			controllerIndex = (controllerIndex+1)%controllers.length;
+			getNextController();
+		});	
+		
+		canvas.addEventListener('prev', function(){
+			generalHandle('prev');
+			getPrev();
+			getNextController();
+		});	
+		
+		function generalHandle(eventName){
+			console.log(eventName);
+			controller.getRight().addMsg(eventName);
+		}
+		
+		function getPrev(){
+			controllerIndex = controllerIndex - 1;
+			if(controllerIndex < 0)
+				controllerIndex = controllers.length -1;
+		}
+		
+		function getNextController(){
+			left = {
+				type: controllers[controllerIndex],
+				args: {}
+			};
+			controller.create(left, right);
+			controller.getRight().addMsg(controllers[controllerIndex]);
+		}
 	};
 	
 }).apply(window.currentGame, [window.thillyLogging]);
